20acf0baf8737fe81a3d25e1cf4519fbffc75190
Deepak Pant <deepak.93p@gmail.com>
2020-04-09 22:21:07 +0530

    adding bootstrap 3 support and created a basic design

--- web/covid19-app/src/app/app.module.ts
+++ web/covid19-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DisplayBoardComponent } from './display-board/display-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DisplayBoardComponent
   ],
   imports: [
     BrowserModule

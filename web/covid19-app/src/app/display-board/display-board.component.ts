33145dbad505479d4cdef921ef67786753b53e9e
Deepak Pant <deepak.93p@gmail.com>
2020-04-10 01:58:11 +0530

    Integrated Covid 19 API

--- web/covid19-app/src/app/display-board/display-board.component.ts
+++ web/covid19-app/src/app/display-board/display-board.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Covid19Service } from '../service/covid19.service';
+import { CovidSummaryModel } from '../model/covid-summary.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-display-board',
   templateUrl: './display-board.component.html',
   styleUrls: ['./display-board.component.css']
 })
-export class DisplayBoardComponent implements OnInit {
+export class DisplayBoardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  totalCases: number;
+  totalDeaths: number;
+  totalRecovered: number;
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(private covidService: Covid19Service) {
   }
 
+  ngOnInit() {
+    this.subscription = this.covidService.covidSummaryModelSubject.subscribe(model => {
+      this.totalCases = model.Global.TotalConfirmed;
+      this.totalDeaths = model.Global.TotalDeaths;
+      this.totalRecovered = model.Global.TotalRecovered;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
